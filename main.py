0711114ce0a8ed50f0d297a82a8e1795f03e337e
Ataxanov <asherzod2197@gmail.com>
2026-04-23 20:38:08 +0500

    Add PhoneBattery class with use and charge methods

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+class PhoneBattery:
+    def __init__(self, percent):
+        self.percent = percent
 
+    def use(self, amount):
+        self.percent -= amount
+        if self.percent <= 0:
+            self.percent = 0
+            print("Quvvat tugadi")
+        else:
+            print(f"Batareya: {self.percent}%")
+
+    def charge(self, amount):
+        self.percent += amount
+        if self.percent > 100:
+            self.percent = 100
+        print(f"Batareya: {self.percent}%")
+
+
+# Obyekt
+phone = PhoneBattery(50)
+phone.use(20)
+phone.use(40)
+phone.charge(30)
